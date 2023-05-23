cd45e8fb19c4705a1199d75085e7a01bad4c0ebc
Sabet Maulana Mubarok <86289370+saabet@users.noreply.github.com>
2023-05-23 18:45:21 +0700

    update table.js

--- components/table.js
+++ components/table.js
@@ -6,28 +6,28 @@ export default function Table(){
         <table className="min-w-full table-auto">
             <thead>
                 <tr className= "bg-gray-800">
-                    <th className="px-16 py-2">
+                    <th className="px-5 py-2">
                         <span className="text-gray-300">id</span>
                     </th>
-                    <th className="px-16 py-2">
+                    <th className="px-5 py-2">
                         <span className="text-gray-300">course_id</span>
                     </th>
-                    <th className="px-16 py-2">
+                    <th className="px-5 py-2">
                         <span className="text-gray-300">module_name</span>
                     </th>
-                    <th className="px-16 py-2">
+                    <th className="px-5 py-2">
                       <span className="text-gray-300">tutorial_id</span>
                     </th>
-                    <th className="px-16 py-2">
+                    <th className="px-5 py-2">
                        <span className="text-gray-300">discussion_title</span>
                     </th>
-                    <th className="px-16 py-2">
+                    <th className="px-5 py-2">
                         <span className="text-gray-300">question</span>
                     </th>
-                    <th className="px-16 py-2">
+                    <th className="px-5 py-2">
                         <span className="text-gray-300">tags</span>
                     </th>
-                    <th className="px-16 py-2">
+                    <th className="px-5 py-2">
                         <span className="text-gray-300">Action</span>
                     </th>
                 </tr>
@@ -44,34 +44,33 @@ export default function Table(){
 function Tr({id, course_id, module_name, tutorial_id, discussion_title, question, tags}){
     return (
         <>
-        <tr>
-                    <td className="px-16 py-2 items-center"> 
-                        <span className="text-center ml-2 font-semibold">{id || "Unknown"}</span>                       
-                    </td>
-                    <td className="px-16 py-2 ">    
-                        <span>{course_id}</span>                       
-                    </td>
-                    <td className="px-16  py-2"> 
-                        <span>{module_name || "Unknown"}</span>                       
-                    </td>
-                    <td className="px-16  py-2"> 
-                        <span>{tutorial_id || "Unknown"}</span>                       
-                    </td>
-                    <td className="px-16  py-2"> 
-                        <span>{discussion_title || "Unknown"}</span>                       
-                    </td>
-                    <td className="px-16  py-2"> 
-                        <span>{question || "Unknown"}</span>                       
-                    </td>
-                    <td className="px-16  py-2"> 
-                        <span>{tags || "Unknown"}</span>                       
-                    </td>
-                    <td className="px-16  py-2"> 
-                        <button className="cursor"><BiEdit size={25}></BiEdit></button>   
-                        <button className="cursor"><BiTrashAlt size={25} ></BiTrashAlt></button>                    
-                 
-                    </td>
-                </tr>
+            <tr>
+                <td className="px-5 py-2 items-center"> 
+                    <span className="text-center ml-2 font-semibold">{id || "Unknown"}</span>                       
+                </td>
+                <td className="px-5 py-2 ">    
+                    <span>{course_id}</span>                       
+                </td>
+                <td className="px-5  py-2"> 
+                    <span>{module_name || "Unknown"}</span>                       
+                </td>
+                <td className="px-5  py-2"> 
+                    <span>{tutorial_id || "Unknown"}</span>                       
+                </td>
+                <td className="px-5  py-2"> 
+                    <span>{discussion_title || "Unknown"}</span>                       
+                </td>
+                <td className="px-5  py-2"> 
+                    <span>{question || "Unknown"}</span>                       
+                </td>
+                <td className="px-5  py-2"> 
+                    <span>{tags || "Unknown"}</span>                       
+                </td>
+                <td className="px-5  py-2"> 
+                    <button className="cursor"><BiEdit size={25}></BiEdit></button>   
+                    <button className="cursor"><BiTrashAlt size={25} ></BiTrashAlt></button>                    
+                </td>
+            </tr>
         </>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,7 @@ export default function Home() {
           </div>
             <form action=''>
               <div className='relative flex items-center text-gray-400 focus-within:text-gray-600'>
-                <input type='text' name='search' placeholder='Search Discussion' autoComplete='off' aria-label='Search Discussion' className='pr-3 pl-10 py-2 font-semibold placeholder-gray-500 text-black rounded-md border-none ring-2 ring-gray-300 focus:ring-gray-500 focus:ring-2 '/>
+                <input type='text' name='search' placeholder='Search Discussion' autoComplete='off' aria-label='Search Discussion' className='pr-3 pl-10 py-2 placeholder-gray-500 text-black rounded-md border-none ring-2 ring-gray-300 focus:ring-gray-500 focus:ring-2 '/>
                 <span className='w-5 h-5 absolute ml-3 pointer-events-none'><BiSearch size={23} /></span>
 
               </div>
