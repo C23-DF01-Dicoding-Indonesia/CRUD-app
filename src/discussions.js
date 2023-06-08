51572b24e5eb9146a9bf3d7d24b39edd29710c11
Sabet Maulana Mubarok <86289370+saabet@users.noreply.github.com>
2023-06-08 20:19:14 +0700

    fixing tags problem

--- src/discussions.js
+++ src/discussions.js
@@ -7,7 +7,7 @@ const discussions = [
     discussion_title: "Apakah inklusif atau eksklusif?",
     question:
       "<p>Saya mengalami error begini:</p><p>- Logika masih salah ketika score mendapatkan nilai antara 70 hingga 79 - Logika masih salah ketika score mendapatkan nilai antara 60 hingga 69 - Logika masih salah ketika score mendapatkan nilai di bawah 60&nbsp;</p><p>dengan petunjuk begini:</p><p>&nbsp;* Buatlah logika if untuk mengevaluasi nilai score dengan ketentuan:</p><p>&nbsp;* &nbsp;1. Jika nilai score lebih atau sama dengan 90</p><p>&nbsp;* &nbsp; &nbsp; &nbsp;- Isi variabel result dengan nilai: 'Selamat! Anda mendapatkan nilai A.'</p><p>&nbsp;* &nbsp;2. Jika nilai score ada di antara 80 hingga 89</p><p>&nbsp;* &nbsp; &nbsp; &nbsp;- Isi variabel result dengan nilai: 'Anda mendapatkan nilai B.'</p><p>&nbsp;* &nbsp;3. Jika nilai score ada di antara 70 hingga 79</p><p>&nbsp;* &nbsp; &nbsp; &nbsp;- Isi variabel result dengan nilai: 'Anda mendapatkan nilai C.'</p><p>&nbsp;* &nbsp;4. Jika nilai score ada di antara 60 hingga 69:</p><p>&nbsp;* &nbsp; &nbsp; &nbsp;- Isi variabel result dengan nilai: 'Anda mendapatkan nilai D.'</p><p>&nbsp;* &nbsp;5. Jika nilai score di bawah 60:</p><p>&nbsp;* &nbsp; &nbsp; &nbsp;- Isi variabel result dengan nilai: 'Anda mendapatkan nilai E.'</p><p><br></p><p>Apakah tidak ada petunjuk tentang inklusif atau ekskulifnya suatu kondisi/angka syarat?</p>",
-    tags: "[conditions]",
+    tags: "conditions",
   },
   {
     id: 2,
@@ -17,7 +17,7 @@ const discussions = [
     discussion_title: "Perbedaan nilai pada Fungsi BigInt",
     question:
       '<p>Halo. kenapa hasil pembuatan tipe data BigInt pakai fungsi BigInt kalau parameter masukannya menggunakan string dan nilai biasa hasilnya berbeda?</p><p><img src="https://dicodingacademy.blob.core.windows.net/academies/20210401063601d2f5b7a61fe8dafa535edfcf536bc2c2.png" style="width: 686px;" class="fr-fic fr-dib" data-success="true"></p><p>Mohon pencerahannya :)&nbsp;</p>',
-    tags: "[data-type]",
+    tags: "data-type",
   },
   {
     id: 3,
@@ -28,7 +28,7 @@ const discussions = [
       "Min, saya sudah submit jawaban kuis if saya tunggu respon 1 jam belum ada hasil ya?",
     question:
       '<p><img src="https://dicodingacademy.blob.core.windows.net/academies/20210401075823d6ed1c5d3bce052737910ce9968d0e40.PNG" style="width: 300px;" class="fr-fic fr-dib" data-success="true"></p>',
-    tags: "[if,infinityloading]",
+    tags: "if, infinityloading",
   },
 ];
 
--- src/handler.js
+++ src/handler.js
@@ -1,25 +1,28 @@
-const { nanoid } = require('nanoid');
-const discussions = require('./discussions');
-const axios = require('axios');
+const { nanoid } = require("nanoid");
+const discussions = require("./discussions");
+const axios = require("axios");
 
 const addDiscussionHandler = async (request, h) => {
   const { discussion_title, question } = request.payload;
   const id = nanoid(3);
   const insertedAt = new Date().toISOString();
-  const q = discussion_title + ' ' + question;
+  const q = discussion_title + " " + question;
   let result;
   const jsonq = {
-    query: q
-  }
+    query: q,
+  };
 
   try {
-    result = await axios.post('https://autotag-api-4mzlrxm2sa-et.a.run.app/discussion', jsonq);
-  }
-  catch (error) {
+    result = await axios.post(
+      "https://autotag-api-4mzlrxm2sa-et.a.run.app/discussion",
+      jsonq
+    );
+  } catch (error) {
     console.error(error);
   }
 
-  const tags = result.data;
+  const tagsresult = result.data.data;
+  const tags = tagsresult.join(", ");
 
   const newDiscussions = {
     id,
@@ -31,8 +34,8 @@ const addDiscussionHandler = async (request, h) => {
 
   if (discussion_title === undefined) {
     const response = h.response({
-      status: 'fail',
-      message: 'Failed to add new discussion. Please enter the title!',
+      status: "fail",
+      message: "Failed to add new discussion. Please enter the title!",
     });
     response.code(400);
     return response;
@@ -40,8 +43,8 @@ const addDiscussionHandler = async (request, h) => {
 
   if (question === undefined) {
     const response = h.response({
-      status: 'fail',
-      message: 'Failed to add new discussion. Please enter the description!',
+      status: "fail",
+      message: "Failed to add new discussion. Please enter the description!",
     });
     response.code(400);
     return response;
@@ -50,11 +53,9 @@ const addDiscussionHandler = async (request, h) => {
   discussions.push(newDiscussions);
 
   const response = h.response({
-    status: 'success',
-    message: 'Discussion is successfully added',
-    data: {
-      tags,
-    },
+    status: "success",
+    message: "Discussion is successfully added",
+    data: tags,
   });
   response.code(201);
   return response;
@@ -67,9 +68,9 @@ const editDiscussionByIdHandler = (request, h) => {
   const index = discussions.findIndex((note) => note.id == id);
 
   if (!discussion_title | !question | !tags) {
-    messages = 'Failed to update the discussion!';
+    messages = "Failed to update the discussion!";
     const response = h.response({
-      status: 'fail',
+      status: "fail",
       message: messages,
     });
     response.code(400);
@@ -89,16 +90,16 @@ const editDiscussionByIdHandler = (request, h) => {
     };
 
     const response = h.response({
-      status: 'success',
-      message: 'Discussion is succesfully updated!',
+      status: "success",
+      message: "Discussion is succesfully updated!",
     });
     response.code(200);
     return response;
   }
 
   const response = h.response({
-    status: 'fail',
-    message: 'Failed to update the discussion! Discussion ID not found.',
+    status: "fail",
+    message: "Failed to update the discussion! Discussion ID not found.",
   });
   response.code(404);
   return response;
@@ -111,15 +112,15 @@ const deleteDiscussionByIdHandler = (request, h) => {
   if (index !== -1) {
     discussions.splice(index, 1);
     const response = h.response({
-      status: 'success',
-      message: 'Discussion is succesfully deleted!',
+      status: "success",
+      message: "Discussion is succesfully deleted!",
     });
     response.code(200);
     return response;
   }
   const response = h.response({
-    status: 'fail',
-    message: 'Failed to delete the discussion! Discussion ID not found.',
+    status: "fail",
+    message: "Failed to delete the discussion! Discussion ID not found.",
   });
 
   response.code(404);
@@ -127,7 +128,7 @@ const deleteDiscussionByIdHandler = (request, h) => {
 };
 
 const getAllDiscussionsHandler = () => ({
-  status: 'success',
+  status: "success",
   data: {
     discussions,
   },
@@ -138,30 +139,32 @@ const searchDiscussionHandler = async (request, h) => {
   let result;
   let arr = [];
   const jsonq = {
-    query: q
-  }
+    query: q,
+  };
 
   try {
-    result = await axios.post('https://search-api-4mzlrxm2sa-et.a.run.app/search', jsonq);
-  }
-  catch (error) {
+    result = await axios.post(
+      "https://search-api-4mzlrxm2sa-et.a.run.app/search",
+      jsonq
+    );
+  } catch (error) {
     console.error(error);
   }
 
-  for (let i = 0; i < result.data.data.length; i++){
+  for (let i = 0; i < result.data.data.length; i++) {
     let temp = {
       id: result.data.data[i].id,
       title: result.data.data[i].title,
       type: result.data.data[i].type,
       content: result.data.data[i].content,
       score: result.data.data[i].score,
-    }
+    };
     arr.push(temp);
   }
 
   const response = h.response({
-    status: 'success',
-    message: 'yeay',
+    status: "success",
+    message: "yeay",
     data: arr,
   });
   return response;
@@ -171,10 +174,10 @@ const getDiscussionByIdHandler = (request, h) => {
   const { id } = request.params;
 
   const discussion = discussions.filter((n) => n.id == id)[0];
-  
+
   if (discussion !== undefined) {
     return {
-      status: 'success',
+      status: "success",
       data: {
         discussion,
       },
@@ -182,8 +185,8 @@ const getDiscussionByIdHandler = (request, h) => {
   }
 
   const response = h.response({
-    status: 'fail',
-    message: 'Discussion is not found',
+    status: "fail",
+    message: "Discussion is not found",
   });
   response.code(404);
   return response;
