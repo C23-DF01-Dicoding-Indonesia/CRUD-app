9cc0741943531889e74e1d3922e04dd0610c1056
Sabet Maulana Mubarok <86289370+saabet@users.noreply.github.com>
2023-06-08 20:17:31 +0700

    fixing backend fetch bug

--- components/DiscussionCard.js
+++ components/DiscussionCard.js
@@ -10,11 +10,11 @@ const DiscussionCards = ({title, content, type, score}) => {
   return (
     <div className="w-full md:w-1/2 lg:w-full xl:w-full p-2">
       <div className="bg-gray-100 p-4 rounded-lg shadow-md">
-        <h2 className="text-xl font-bold py-2">{title}</h2>
-        <p className="text-xl text-gray-700 mb-4">{content}</p>
+        <h2 className="text-base font-bold py-2">{title}</h2>
+        <p className="text-base text-gray-700 mb-4">{content}</p>
         <div className="flex flex-wrap">
-          <span className="text-gray-700 text-base mr-2 mb-2 bg-slate-300 px-2 rounded-md">{type}</span>
-          <span className="text-gray-700 text-base mr-2 mb-2 bg-slate-300 px-2 rounded-md">Relevance score: {score}</span>
+          <span className="text-gray-700 text-sm mr-2 mb-2 bg-slate-300 px-2 rounded-md">{type}</span>
+          <span className="text-gray-700 text-sm mr-2 mb-2 bg-slate-300 px-2 rounded-md">Relevance score: {score}</span>
         </div>
       </div>
     </div>
--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -3,7 +3,6 @@ import SearchInput from '@/components/SearchInput';
 import Head from 'next/head';
 import { useSearchParams } from 'next/navigation';
 import DiscussionCard from '@/components/DiscussionCard';
-import data from '@/database/data.json';
 import {useState, useEffect} from 'react';
 import { useRouter } from 'next/navigation';
 
@@ -11,13 +10,8 @@ import { useRouter } from 'next/navigation';
 const SearchPage = () => {
 
     const [searchResults, setSearchResults] = useState(null);
-    const discussion = data;
     const search = useSearchParams();
     const router = useRouter();
-
-    const onclick = () => {
-        router.push(`/`);
-    };
 
     useEffect(() => {
         const fetchData = async () => {
@@ -25,7 +19,7 @@ const SearchPage = () => {
             const searchQuery = search ? search.get('q') : null;
             const encodedSearchQuery = encodeURI(searchQuery || '');
 
-            const response = await fetch(`http://localhost:9000/search?q=${encodedSearchQuery}`);
+            const response = await fetch(`https://deploy-project-389113.et.r.appspot.com/search?q=${encodedSearchQuery}`);
             const results = await response.json();
             setSearchResults(results.data);
 
@@ -46,7 +40,7 @@ const SearchPage = () => {
                 <meta name='viewport' content='width=device-width, initial-scale=1' />
             </Head>
             <main className="py-5">
-                <h1 onClick={onclick} className="text-xl md:text-5xl text-center font-bold py-10">CRUD App Discussion Forum</h1>
+                <h1 className="text-xl md:text-5xl text-center font-bold py-10">CRUD App Discussion Forum</h1>
                 <div className="container mx-auto flex justify-end py-5">
                     <SearchInput/>
                 </div>
